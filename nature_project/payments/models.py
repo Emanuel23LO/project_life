e632fb8e57fe49084ad3a68adda6e24f79517c37
Jader1709 <jaderbolivar80@gmail.com>
2024-02-28 10:23:06 -0500

    payments

--- nature_project/payments/models.py
+++ nature_project/payments/models.py
@@ -1,27 +1,13 @@
 from django.db import models
-from django.utils import timezone
 
 
 class Payment(models.Model):
     payment_method = models.CharField(max_length=255)
-    date = models.DateField(default=timezone.now)
-    value = models.FloatField(max_length=500)
+    date = models.DateField()
+    value = models.FloatField(max_length=50)
     booking = models.ForeignKey('bookings.Booking', on_delete=models.DO_NOTHING)
     status = models.BooleanField(default=True)
 
-    def __str__(self):
-        return str.booking
-
-    def save(self, *args, **kwargs):
-     # Set date_booking only if it's not already set
-        if not self.date_booking:
-            self.date_booking = timezone.now()
-        super().save(*args, **kwargs)
-
-
-
-
-
-
-
+def __str__(self):
+    return self.value
 
--- nature_project/payments/views.py
+++ nature_project/payments/views.py
@@ -2,29 +2,10 @@ from django.shortcuts import render, redirect
 from payments.models import Payment
 from django.http import JsonResponse
 from django.contrib import messages
+
+
+
 from .forms import PaymentForm
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from bookings.models import Booking
-
-
-
-
-@receiver(post_save, sender=Payment)
-def status_reservation(sender, instance, created, **kwargs):
-    booking = Booking.objects.get(pk=instance.booking_id)
-    valor_reserva = booking.value
-    if instance.value > 0.5 * valor_reserva:
-        texto = 'pagado'
-    elif instance.value >= 0.5:
-        texto = 'confirmado'
-    elif instance.value <= 0:
-        texto = 'cancelado'
-    else:
-        texto= 'reservado'
-
-    Booking.objects.filter(id=instance.booking_id).update(status=texto)
-    
 def payments(request):    
     payments_list = Payment.objects.all()    
     return render(request, 'payments/index.html', {'payments_list': payments_list})
@@ -47,20 +28,9 @@ def create_payment(request):
     return render(request, 'payments/create.html', {'form': form})
 
 def detail_payment(request, payment_id):
-    try:
-        payment = Payment.objects.get(pk=payment_id)
-        data = {
-            'payment_method': payment.payment_method,
-            'date': payment.date,
-            'value': payment.value,
-        }
-        return JsonResponse(data)
-    except Payment.DoesNotExist:
-        messages.error(request, 'El pago especificado no existe.')
-        return JsonResponse({'error': 'Payment not found'}, status=404)
-    except Exception as e:
-        messages.error(request, f'Ocurrió un error al obtener los detalles del pago: {str(e)}')
-        return JsonResponse({'error': str(e)}, status=500)
+    payment = Payment.objects.get(pk=payment_id)
+    data = { 'payment_method': payment.payment_method, 'date': payment.date, 'value': payment.value }
+    return JsonResponse(data)
 
 def delete_payment(request, payment_id):
     payment = Payment.objects.get(pk=payment_id)
